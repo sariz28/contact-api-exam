1fe7cb5423dee3a82f10b2c04dd03b63480c3ea3
sariz28 <sariz.sanchez@gmail.com>
2023-02-22 13:52:35 -0600

    feat: add swagger dependencie and document api

--- src/main/java/com/involverh/exam/controller/ContactController.java
+++ src/main/java/com/involverh/exam/controller/ContactController.java
@@ -2,6 +2,8 @@ package com.involverh.exam.controller;
 
 import com.involverh.exam.model.Contact;
 import com.involverh.exam.repository.ContactRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +16,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/contact")
+@Api(value="/contact", tags = "Contact Service")
 public class ContactController {
 
     @Autowired
     private ContactRepository contactRepository;
 
     @GetMapping("/")
+    @ApiOperation(value = "Provides all contacts")
     public ResponseEntity<List<Contact>> getContacts() {
 
         List<Contact> contacts = contactRepository.findAll();
@@ -27,6 +31,7 @@ public class ContactController {
     }
 
     @GetMapping("/name/{name}")
+    @ApiOperation(value = "Provides contacts by name")
     public ResponseEntity<List<Contact>> getContactsByName(@PathVariable String name) {
 
         List<Contact> contacts = contactRepository.findByNameContainingIgnoreCase(name);
@@ -34,6 +39,7 @@ public class ContactController {
     }
 
     @GetMapping("/phone/{phone}")
+    @ApiOperation(value = "Provides contacts by phone number")
     public ResponseEntity<List<Contact>> getContactsByPhone(@PathVariable String phone) {
 
         List<Contact> contacts = contactRepository.findByPhoneNumberNumberContaining(phone);
